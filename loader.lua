7b5e28d0cb209b5ec9b5abb678e445ccb4d26f29
JumaNexus <merry.bezy8@gmail.com>
2026-01-03 16:42:47 +0700

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,11 @@
+local placeId = game.PlaceId
+
+local games = {
+    [101953168527257] = "https://raw.githubusercontent.com/JumaNexus/Spear-Fishing/refs/heads/main/main.lua",
+    [92122513197996] = "https://raw.githubusercontent.com/JumaNexus/Hub/refs/heads/main/dte.lua",
+    [96645548064314] = "https://raw.githubusercontent.com/JumaNexus/Catch-And-Tame/refs/heads/main/main.lua"
+}
+
+if games[placeId] then
+    loadstring(game:HttpGet(games[placeId]))()
+end
