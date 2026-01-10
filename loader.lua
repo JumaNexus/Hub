b400e007d5c76718b96b48cbf5c33309ed61b828
JumaNexus <merry.bezy8@gmail.com>
2026-01-11 04:00:44 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,7 +3,8 @@ local placeId = game.PlaceId
 local games = {
     [101953168527257] = "https://raw.githubusercontent.com/JumaNexus/Spear-Fishing/refs/heads/main/main.lua",
     [92122513197996] = "https://raw.githubusercontent.com/JumaNexus/Hub/refs/heads/main/dte.lua",
-    [96645548064314] = "https://raw.githubusercontent.com/JumaNexus/Catch-And-Tame/refs/heads/main/main.lua"
+    [96645548064314] = "https://raw.githubusercontent.com/JumaNexus/Catch-And-Tame/refs/heads/main/main.lua",
+    [109008444518106] = "https://raw.githubusercontent.com/JumaNexus/Hub/refs/heads/main/bap.lua",
 }
 
 if games[placeId] then
